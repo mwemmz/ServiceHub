d31e9b2b3224d21c674508243d71fa0afd765431
chimwemwe <chimwemwesiyingwa361@gmail.com>
2026-09-24 08:50:06 +0200

    Render web build as client-side SPA to eliminate hydration errors

--- app.config.js
+++ app.config.js
@@ -48,7 +48,7 @@ export default {
     },
     web: {
       bundler: 'metro',
-      output: 'static',
+      output: 'single',
       favicon: './assets/images/favicon.png',
     },
     plugins: [
